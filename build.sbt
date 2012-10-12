94e675ab82579b5e5478497925317a4ceab2a8e3
Alexis Agahi <alag@users.sf.net>
2012-10-12 23:51:35 +0200

    Refactor Code + add runtime AST compilation

--- build.sbt
+++ build.sbt
@@ -1,11 +1,15 @@
 scalaVersion := "2.10.0-RC1"
 
 
+
+
 scalaSource in Compile <<= baseDirectory(_ / "src")
 
 scalaSource in Test <<= baseDirectory(_ / "test")
 
 libraryDependencies += "com.novocode" % "junit-interface" % "0.10-M1" % "test"
 
-
 libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.10.0-RC1"
+
+libraryDependencies += "org.scala-lang" % "scala-compiler" % "2.10.0-RC1"
+
--- test/DynamicTest.scala
+++ test/DynamicTest.scala
@@ -1,38 +1,13 @@
 import org.junit.Test
 
-import MyMacros._
 
 class DynamicTest {
 
   @Test
   def ahoy() {
-    val myDynamicStuff = MyDynamic("ahoy")
-    assert(myDynamicStuff.ahoy == "ahoy Duke")
-    assert(myDynamicStuff.plouf == "plouf Duke")
-    //println(MyDynamicStuff.ahoy("Today is a good day !!"))
+    val duke = DynamicDuke("ahoy")
+    assert(duke.ahoy == "ahoy Duke")
+    assert(duke.plouf == "plouf Duke")
   }
 
-  @Test
-  def desugarTest() {
-    println(desugar((1 to 10).map(_ + 1)))
-
-    println(desugar((_:String) => {
-      println("ahoy")
-      "duke"
-    }))
-  }
-
-  @Test
-  def logIsAllMyLife() {
-    val plouf =
-
-    log(log("plouf".toUpperCase).contains("P"))
-  }
-
-  @Test
-  def binaryDude() {
-
-    println(b"010011")
-  }
-
-  }
+}
